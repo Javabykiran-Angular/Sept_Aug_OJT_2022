e485122b288ebcf2bf633d0053f0980091175891
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-12-27 15:55:39 +0530

    Day13

---
+++ Angular/Day13/course/course.service.ts
@@ -0,0 +1,12 @@
+
+export class CourseService{
+
+    private course:string[]=['Core java','Advanced Java','Spring Boot','JSP','Angular 12','Docker','AWS','DevOps'];
+
+
+    getCourse(){
+        return (this.course);
+    }
+
+
+}
