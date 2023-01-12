061232f5ef19b57ab6883a0d27b188f036e268d8
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-01-12 16:07:34 +0530

    Day25

---
+++ Angular/Day25/details/details.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpService } from '../http.service';
+import { Employee } from '../model/employee';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent implements OnInit {
+
+  empObj=<Employee>{}
+  constructor(private route:ActivatedRoute,
+              private service:HttpService) { }
+
+  ngOnInit(): void {
+    this.getDataFromUrl();
+  }
+  getDataFromUrl(){
+      this.route.paramMap
+      .subscribe((param:any)=>{
+        // console.log(param.get("id"))
+        this.service.getParticularRecord(+param.get("id"))
+        .subscribe((response)=>{
+          // console.log(response)
+          this.empObj=(<Employee>response)
+          console.log("====="+this.empObj);
+        })
+      })
+  }
+
+}
